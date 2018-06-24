fce8db02f10196780618a4f2c0557ac4b21bc6a2
Haydn Evans <haydn_j_evans@hotmail.com>
2018-06-24 20:48:35 +1000

    fix syntax

--- hostfilewrapper.sh
+++ hostfilewrapper.sh
@@ -1,23 +1,21 @@
 #!/bin/bash -e  
   
+#Stops the network manager and unlocks the dnsmasq bin
+  
 systemctl stop NetworkManager  
   
 #The first command copies the dnsmasq binary to "dnsmasq.bin" (basically the same)  
   
 mv /usr/sbin/dnsmasq /usr/sbin/dnsmasq.bin  
   
-#We then create a brand new "dnsmasq" which is actually a bash script  
+#We then create a brand new "dnsmasq" which is actually a bash script we have prepared 
 
-/bin/cat > /usr/sbin/dnsmasq' <<EOF  
-	
-	DNSMASQ_BIN="/usr/sbin/dnsmasq.bin"  
-	# strip --no-hosts switch so /etc/hosts is read  
-	DNSMasqParameters=$(echo -n "$@" | sed --regexp-extended "s/ --no-hosts( |$)/ /")  
-	exec $DNSMASQ_BIN $DNSMasqParameters  
-EOF  
+cp /var/git/hostfilewrapper/dnsmasq > /usr/sbin/dnsmasq 
 
 #Set the correct permissions for the script
 
 chmod +x /usr/sbin/dnsmasq  
   
+#restarts the network manager 
+
 systemctl start NetworkManager  
